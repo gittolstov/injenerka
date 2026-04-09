c6397b8afd503542e4aa0c31c78456a39ad634a1
gittolstov <git.tolstov@gmail.com>
2026-04-09 19:05:03 +0300

    increase brightness

---
+++ pmw-led.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+from math import log10
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+button = 13
+photo = 6
+
+state = 1
+duty = 0.0
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(photo, GPIO.IN)
+
+pwm = GPIO.PWM(led, 200)
+pwm.start(duty)
+
+while True:
+    pwm.ChangeDutyCycle(10 ^ (duty / 50))
+    time.sleep(0.05)
+
+    duty += 1
+    if duty >= 100:
+        duty = 0
